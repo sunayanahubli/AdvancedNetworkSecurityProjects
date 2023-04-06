88fff529339342dd3ef2159f3f26ef08315d4ee7
sunayanahubli <sunayanahubli@gmail.com>
2023-04-06 16:10:24 -0400

    Added SSL for server to client communication

--- Project_01/client_test.py
+++ Project_01/client_test.py
@@ -2,50 +2,65 @@ import logging
 import socket
 import threading
 import time
+import ssl
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 logger = logging.getLogger(__name__)
 FORMAT = '%(asctime)server %(name)-15.5s %(threadName)-10s %(levelname)-8s %(message)server'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
-list_of_clients = []
 server = socket.socket()
+list_of_clients = []
 client_name = "Client1"
 client_channel_port = 9994
-import ssl
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from Crypto.Util.Padding import unpad
-from json import dumps, loads
-from requests import post as post_request
+
 
 def ping():
+    # Set up AES encryption key and IV
+    key = b'0123456789abcdef'
+    iv = b'fedcba9876543210'
+
     message = client_name + ": PING"
     message = pad(str.encode(message), 16)
+
     while True:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
         fetch_ip_adresses()
+        time.sleep(5)
         try:
             for client in list_of_clients:
                 if client['ip_address'] != '' and not (client_name) in client['name']:
                     sock.connect((client['ip_address'], client_channel_port))
-                    sock.sendall(message)
+
+                    # Encrypt the message using AES
+                    cipher = AES.new(key, AES.MODE_CBC, iv)
+                    encrypted_message = cipher.encrypt(message)
+
+                    sock.sendall(encrypted_message)
                     print(unpad(message, 16).decode())
+
+                    # Receive the encrypted response from the client and decrypt it using AES
                     data = sock.recv(1024)
-                    print(str(unpad(data,16).decode()))
-                    sock.close()
+                    cipher = AES.new(key, AES.MODE_CBC, iv)
+                    decrypted_response = unpad(cipher.decrypt(data), 16).decode()
+                    print(str(decrypted_response))
 
         except Exception as e:
-            logger.info(e)
+            logger.error(e)
 
         finally:
-            time.sleep(15)
-            # print('Ping session complete')
-    sock.close()
+            time.sleep(10)
+
 
 def pong():
+    # Set up AES encryption key and IV
+    key = b'0123456789abcdef'
+    iv = b'fedcba9876543210'
+
     message = client_name + ": PONG"
     message = pad(str.encode(message), 16)
-    # print("Trying to Pong")
 
     while True:
         try:
@@ -53,59 +68,63 @@ def pong():
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sock.bind(('127.0.0.1', client_channel_port))
             sock.listen(15)
+
             (client_socket, client_address) = sock.accept()
             data = client_socket.recv(2048)
+
             if data:
-                print((str(unpad(data, 16).decode())))
-                client_socket.send(message)
+                # Decrypt the received message using AES
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                decrypted_message = unpad(cipher.decrypt(data), 16).decode()
+
+                print(str(decrypted_message))
+
+                # Encrypt the response message using AES and send it back to the client
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                encrypted_response = cipher.encrypt(message)
+                client_socket.sendall(encrypted_response)
+
                 print(unpad(message, 16).decode())
+
         except socket.error as e:
-            print("Socket error:", e)
+            logger.error("Socket error:", e)
+
         finally:
             time.sleep(2)
-    sock.close()
 
-
-def connect_to_server():
-    # logger.info("Connection to Server.py Thread")
+def connect_to_server(client_name = "Client1"):
+    logger.info("Connection to Server.py Thread")
     host = 'localhost'
     port2 = 9995
-    server.connect((host, port2))
-    context = ssl.create_default_context()
-    context.check_hostname = True
-    context.verify_mode = ssl.CERT_REQUIRED
-    context.load_verify_locations(cafile="server.crt")
-    s = socket.socket()
-    ssl_sock = context.wrap_socket(s, server_hostname='localhost')
+    global sock_ssl
+    sock_ssl= ssl.wrap_socket(server, cert_reqs=ssl.CERT_REQUIRED, ca_certs='server.crt')
+    sock_ssl.connect((host, port2))
     print(f"Registered with name: {client_name}")
     while True:
-        server.sendall(pad(str.encode(client_name), 16))
+        sock_ssl.sendall(pad(str.encode(client_name), 16))
         time.sleep(5)
 
+def fetch_ip_adresses(data=None):
+    data=''
+    while((data)==''):
+        data = sock_ssl.recv(2048).decode("utf-8")
+        if len(data) > 1:
+            print (data)
+            print(len(data))
+            ips = data.strip().split(',')
+            if len(ips) > 0:
+                list_of_clients.clear()
+                for ip in ips:
+                    if len(ip) > 0:
+                        print(ip)
+                        client = {'name': ip.split(':')[1], 'ip_address': ip.split(':')[0]}
+                        if ' ' not in client['name']:
+                            list_of_clients.append(client)
 
-
-def fetch_ip_adresses():
-    # print("Fetch ip")
-    list_of_clients.clear()
-    data = server.recv(2048)
-    if len(data) > 0:
-        data1 = str(data.decode("utf-8"))
-        # print("Data" + data1)
-        ips = data1.strip().split(',')
-        if len(ips) > 0:
-            for ip in ips:
-                # print(ip)
-                client = {'name': ip.split(':')[1], 'ip_address': ip.split(':')[0]}
-                if ' ' not in client['name']:
-                    list_of_clients.append(client)
-
-        logger.info(f'Current list of clients: {(list_of_clients)}')
-
-
-# connect_to_server(client_name)
+            logger.info(f'Current list of clients: {(list_of_clients)}')
 
 # # logger.info(f'spawning before')
-server_thread = threading.Thread(target=connect_to_server)
+server_thread = threading.Thread(target=connect_to_server, args=(client_name,))
 server_thread.start()
 time.sleep(10)
 ping_thread = threading.Thread(target=ping)
--- Project_01/server_test.py
+++ Project_01/server_test.py
@@ -26,8 +26,7 @@ def create_socket():
         host = ""
         port = 9995
         s = socket.socket()
-        ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        ssl_context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+
 
     except socket.error as msg:
         print("Socket creation error: " + str(msg))
@@ -47,46 +46,73 @@ def bind_socket():
         print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
         bind_socket()
 
+# def accepting_connections():
+#     for c in all_connections:
+#         c.close()
+#     del all_connections[:]
+#     del all_address[:]
+#
+#     while True:
+#             client = {}
+#         # try:
+#             conn, address = s.accept()
+#             s.setblocking(1)  # prevents timeout
+#
+#             all_connections.append(conn)
+#             all_address.append(address)
+#             data = conn.recv(1024)
+#             client['name'] = unpad(data,16).decode()
+#             client['addr']= address[0]
+#             client['con']= conn
+#             results.append(client)
+#             print('Client Registered with name "%s"' % client['name'])
+#             print(client)
+#         # except:
+#         #      print("Error accepting connections")
+
 def accepting_connections():
     for c in all_connections:
         c.close()
     del all_connections[:]
     del all_address[:]
+    sock_ssl = ssl.wrap_socket(s, server_side=True, certfile='server.crt', keyfile='server.key')
+    sock_ssl.listen()
 
     while True:
         client = {}
         try:
-            conn, address = s.accept()
-            s.setblocking(1)  # prevents timeout
+            conn, address = sock_ssl.accept()
+            sock_ssl.setblocking(1)  # prevents timeout
 
             all_connections.append(conn)
             all_address.append(address)
-            data = conn.recv(1024)
-            client['name'] = unpad(data,16).decode()
-            client['addr']= address[0]
-            client['con']= conn
+            data = conn.recv(2048)
+            if len(data) % 16 != 0:
+                print(len(data) )
+                continue
+            client['name'] = unpad(data, 16).decode()
+            client['addr'] = address[0]
+            client['con'] = conn
             results.append(client)
             print('Client Registered with name "%s"' % client['name'])
             print(client)
-        except:
-             print("Error accepting connections")
-
+        except Exception as e:
+            print("Error accepting connections:", e)
 
 def list_connections():
      for i, conn in enumerate(results):
          try:
              (conn['con']).send(str.encode(' '))
-
          except:
              print('Exception')
-             del all_connections[i]
-             del all_address[i]
-             del results[i]
+             # del all_connections[i]
+             # del all_address[i]
+             # del results[i]
              continue
 
 def send_list_connections():
     while True:
-        time.sleep(10)
+        time.sleep(5)
         list_connections()
         dummy = []
         for conn in results:
@@ -129,3 +155,4 @@ def create_jobs():
 create_workers()
 
 create_jobs()
+
